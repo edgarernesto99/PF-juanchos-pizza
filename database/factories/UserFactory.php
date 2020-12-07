8e8fc48cfbe950a936e413a5118406c7b4a2305d
Edgar Guzman <edgar.guzman6009@alumnos.udg.mx>
2020-12-07 03:09:29 -0600

    Seeder y factory para User y Persona

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use App\Models\Persona;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -29,5 +30,19 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
+    }
+    public function usuario() {
+        return $this->state(function (array $attributes) {
+            return [
+                'tipo'=>'usuario'
+            ];
+        });
+    }
+    public function administrador() {
+        return $this->state(function (array $attributes) {
+            return [
+                'tipo'=>'administrador'
+            ];
+        });
     }
 }
